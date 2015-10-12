42ce8293b9de25323f4f7ac55d7257201c07fcd8
jessica joly & julianna green <student@epicodus.com>
2015-10-12 11:42:36 -0700

    add edit view and update functionality

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,4 +8,37 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     render :show
   end
+
+  def new
+    @category = Category.new
+    render :new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @category= Category.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to categories_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
